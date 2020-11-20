d4d445ec19782dcf621f4a77311286a22038ee46
lukasdebski <lukasdebski87@gmail.com>
2020-11-20 21:12:38 +0100

    add component HomeAboutUs

---
+++ src/components/Home/HomeAboutUs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomeAboutUs = () => {
+    return(
+        <>
+        <section className="about">
+            <div className="about__box about__bg">
+                <p className='about__title'>O nas</p>
+                <div className="about__decoration"></div>
+                <div className='about__text'>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</div>
+            </div>
+            <div className="about__box about__img"></div>
+        </section>
+        </>
+    )
+}
+
+export default HomeAboutUs;
