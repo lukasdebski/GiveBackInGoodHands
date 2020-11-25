9a9fb12a06f6590c3516566690d4f50c596a95f7
lukasdebski <lukasdebski87@gmail.com>
2020-11-25 10:34:29 +0100

    ContactUs step 2

--- src/components/Home/HomeContactUs.js
+++ src/components/Home/HomeContactUs.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const HomeContactUs = () => {
 
-    const handleSubmit = event => {
-        event.preventDefault();
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleName = e => {
+        setName(e.target.value)
+    }
+
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+
+    const handleMessage = e => {
+        setMessage(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
         alert('You have submitted the form.')
     }
 
@@ -17,17 +33,18 @@ const HomeContactUs = () => {
                         <div className="contact__inputs">
                             <label className='contact__label'>
                                 <p className='contact__text'>Wpisz swoje imię</p>
-                                <input className='contact__el el--in' type="text" name='name' placeholder='imię'/>
+                                <input className='contact__el el--in' type="text" placeholder='imię' onChange={handleName}/>
                             </label>
                             <label className='contact__label'>
                                 <p className='contact__text'>Wpisz swój e-mail</p>
-                                <input className='contact__el el--in' type="text" name='email'
-                                       placeholder='xzcv@zxc.zx'/>
+                                <input className='contact__el el--in' type="text"
+                                       placeholder='xzcv@zxc.zx' onChange={handleEmail}/>
                             </label>
                         </div>
                         <label className='contact__labelText'>
                             <p className='contact__text'>Wpisz swoją wiadomość</p>
-                            <textarea className='contact__area' name="" id="" cols="42" rows="8"
+                            <textarea className='contact__area'
+                                      onChange={handleMessage}
                                       placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'></textarea>
                         </label>
                         <div className="contact__btn">
@@ -35,13 +52,13 @@ const HomeContactUs = () => {
                         </div>
                     </form>
                 </div>
-                <footer className="footer">
-                    <span className='footer__text'>CopyRight by CodersLab</span>
-                    <div className="icons">
-                        <span className="facebook"></span>
-                        <span className="instagram"></span>
-                    </div>
-                </footer>
+                {/*<footer className="footer">*/}
+                {/*    <span className='footer__text'>CopyRight by CodersLab</span>*/}
+                {/*    <div className="icons">*/}
+                {/*        <span className="facebook"></span>*/}
+                {/*        <span className="instagram"></span>*/}
+                {/*    </div>*/}
+                {/*</footer>*/}
             </section>
         </>
     )
